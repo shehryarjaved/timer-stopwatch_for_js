a0c8bcf34d243e1505cd7f1d9400366d1e5ee311
shehryarjaved <shehryarjaved007@gmail.com>
2021-01-31 01:18:45 +0500

    timer,stopwatch on js done

---
+++ app.js
@@ -0,0 +1,50 @@
+var min=0;
+var sec=0;
+var msec=0;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var miliHeading = document.getElementById("mili");
+var interval;
+function timer()
+{
+msec++;
+miliHeading.innerHTML=msec;
+if(msec>= 100)
+{
+sec++;
+secHeading.innerHTML=sec;
+msec=0;
+}
+else if(sec>=60)
+{
+min++;
+minHeading.innerHTML=min;
+sec=0;
+}
+}
+
+function start()
+{
+    interval=setInterval(timer,10);
+    document.getElementById("start").disabled = true;
+
+  
+}
+function stop()
+{
+
+clearInterval(interval);
+document.getElementById("start").disabled = false;  
+}
+function reset()
+{
+min=0;
+sec=0;
+msec=0;
+hour=0;
+minHeading.innerHTML=min;
+secHeading.innerHTML=sec;
+miliHeading.innerHTML=msec;
+stop();
+}
